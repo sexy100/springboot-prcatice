e324858960f87e79ba148c5236b599dadea9f703
Baek <zeras913@gmail.com>
2020-02-20 00:29:17 +0900

    set lombok

--- src/main/java/com/sexy100/springboot/web/HelloController.java
+++ src/main/java/com/sexy100/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.sexy100.springboot.web;
 
+import com.sexy100.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,13 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(
+            @RequestParam("name") String name
+            , @RequestParam("amount") int amount
+    ) {
+        return new HelloResponseDto(name, amount);
     }
 }
