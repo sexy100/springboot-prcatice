b9e474d074aadbc002ad276a01ce41f103e692fb
Baek <zeras913@gmail.com>
2020-03-06 00:30:16 +0900

    add jpa auditing

--- src/main/java/com/sexy100/springboot/Application.java
+++ src/main/java/com/sexy100/springboot/Application.java
@@ -1,9 +1,11 @@
 package com.sexy100.springboot;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 import static org.springframework.boot.SpringApplication.run;
 
-
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
 
--- src/main/java/com/sexy100/springboot/domain/posts/Posts.java
+++ src/main/java/com/sexy100/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.sexy100.springboot.domain.posts;
 
+import com.sexy100.springboot.domain.BaseTimeEntitiy;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntitiy {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
