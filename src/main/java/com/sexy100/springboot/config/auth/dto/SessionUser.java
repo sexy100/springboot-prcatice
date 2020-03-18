548f97ae4d9cc79986a7c09aed4cee0c7058b904
Baek <zeras913@gmail.com>
2020-03-19 00:27:02 +0900

    add oauth in progress

---
+++ src/main/java/com/sexy100/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.sexy100.springboot.config.auth.dto;
+
+import com.sexy100.springboot.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user){
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+
+    }
+
+}
